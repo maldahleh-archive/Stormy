e3797fdc62a6e9feed17cc069d2c3d7a56b4511d
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-09 02:40:43 -0500

    File cleanup

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -14,16 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentPrecipitationLabel: UILabel!
     @IBOutlet weak var currentWeatherIcon: UIImageView!
     @IBOutlet weak var currentSummaryLabel: UILabel!
+    
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
