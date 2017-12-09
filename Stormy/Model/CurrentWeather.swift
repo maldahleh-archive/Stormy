8d4351ad5c455bc0990e7f4802eb6f5456a9e8a8
Mohammed Al-Dahleh <maldahleh@gmail.com>
2017-12-09 00:21:06 -0500

    Implement weather model

---
+++ Stormy/Model/CurrentWeather.swift
@@ -0,0 +1,17 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Mohammed Al-Dahleh on 2017-12-09.
+//  Copyright © 2017 Mohammed Al-Dahleh. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let temperature: Double
+    let humidity: Double
+    let precipitationProbability: Double
+    let summary: String
+    let icon: String
+}
